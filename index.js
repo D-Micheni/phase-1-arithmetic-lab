925b7e330c47e672f8441470d5c9d4403e9c2012
D-Micheni <dennis.micheni@student.moringaschool.com>
2022-08-17 22:02:06 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-// Write your code here
+// Write your code here!
+let num1=2;
+let num2=31;
+let multiply=num1*num2;
+
+const random = Math.floor(Math.random()*10);
+
+let num3=16;
+let num4=6;
+let mod=num3%num4
+
+let num5=4;
+let num6=8;
+let num7=16;
+let num8=20;
+
+
+const max = Math.max(num5, num6, num7, num8,);
